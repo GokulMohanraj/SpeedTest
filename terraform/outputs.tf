aa038df85bade8d035f44119b530f3008ca8db77
Gokul Mohanraj <thegokulraj@gmail.com>
2025-07-01 17:32:35 +0530

    Changes in terraform output.tf file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,9 +7,3 @@ output "instance_id" {
   description = "The ID of the instance"
   value       = aws_instance.speedtest.id
 }
-
-output "name" {
-  description = "The name of the security group"
-  value       = aws_security_group.speedtest_sg.name
-  
-}
