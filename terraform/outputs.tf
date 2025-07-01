1e1ce9e490b76b51edadfc423a6a6d9bd79d6772
Gokul Mohanraj <thegokulraj@gmail.com>
2025-07-01 18:06:05 +0530

    Changes in infra.yml file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "instance_id" {
   description = "The ID of the instance"
   value       = aws_instance.speedtest.id
 }
+
+output "security_group_id" {
+  description = "The ID of the security group"
+  value       = aws_security_group.speedtest_sg.id
+}
