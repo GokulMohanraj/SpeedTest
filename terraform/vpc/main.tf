bd7d575d5d6b80a1a182321370e46b2eace502c2
Ashwini012000 <Ashwini.s-katageri@hp.com>
2025-07-08 11:31:05 +0530

    added the vpc folder

---
+++ terraform/vpc/main.tf
@@ -0,0 +1,34 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "speedtest_sg" {
+  name        = var.security_group_name
+  description = "Security group for Speedtest EC2 instance"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
