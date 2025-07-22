25fe7e391fac37e188fbc280cd52aba0dbb149c7
GokulMohanraj <thegokulraj@gmail.com>
2025-07-22 04:49:09 +0000

    Fix: Changes made in terragrunt plan stage

--- infra/modules/vpc/provider.tf
+++ infra/modules/vpc/provider.tf
@@ -1,9 +1,3 @@
 provider "aws" {
   region = var.aws_region
 }
-
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-  default     = "ap-south-1"
-}
--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -3,3 +3,9 @@ variable "security_group_name" {
   type        = string
   default     = "speedtest-sg" 
 }
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ap-south-1"
+}
